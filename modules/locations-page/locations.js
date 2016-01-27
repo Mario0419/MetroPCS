19ac828d547bb9db98140ab025cb51026c17b7ab
Mario <mario.massad@gmail.com>
2016-01-27 17:16:16 -0500

    updated latest version

--- modules/locations-page/locations.js
+++ modules/locations-page/locations.js
@@ -1,8 +1,18 @@
 Polymer({
   is: 'locations-page',
   clickItem: function(event) {
+    var ph = document.getElementById("pinehillsmarker");
+    var am = document.getElementById("americanamarker");
+    var ec = document.getElementById("eastcolonialmarker");
+    ph.open = false;
+    am.open = false;
+    ec.open = false;
+    
     var markerName = event.currentTarget.attributes.marker.value;
     var marker = document.getElementById(markerName);
-    marker.open = !marker.open;
+    var dest = event.currentTarget.attributes.address;
+    var destination = document.getElementById("destinationAddressInput");
+    destination.value = dest.value;
+    marker.open = true;
   }
 })
