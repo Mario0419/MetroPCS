be6857e3cd28fe8625362060672ee62d34e85d95
David Sierra <david.sierra@rbc.com>
2016-02-04 19:58:09 -0500

    fixed horizontal overflow on home tab

--- modules/home-page/home.js
+++ modules/home-page/home.js
@@ -1,3 +1,13 @@
 Polymer({
   is: 'home-page',
 })
+
+$(document).ready(function() {
+  $('.single-item').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    slidesToShow: 1,
+    arrows:false,
+  });
+});
